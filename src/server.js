c7a0acfe2ac8e7343022af0352158daac107ea6e
soph-iest <sophie.a.debs@gmail.com>
2019-10-23 15:21:42 -0400

    further server setup + helloworld

---
+++ src/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const PORT = 7674;
+
+let server;
+
+const start = async () => {
+    const Hapi = require('hapi');
+    server = Hapi.server({ port: PORT });
+    await server.start();
+    console.log(`Running on port ${PORT}!`); // eslint-disable-line no-console
+};
+
+const registerRoutes = () => {
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (req, h) => 'Hello, world!'
+    });
+};
+
+start();
+registerRoutes();
