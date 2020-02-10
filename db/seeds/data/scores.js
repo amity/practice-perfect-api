62f55bceb5c0e195dba2c3b162340841005b3e65
soph-iest <sophie.a.debs@gmail.com>
2020-02-10 00:44:17 -0500

    intiial jest config

---
+++ db/seeds/data/scores.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+      user: 1,
+      song: 1,
+      score: 98,
+      mode: 0,
+      date: '2019-11-14',
+      deleted: false
+    },
+    {
+      user: 1,
+      song: 2,
+      score: 98,
+      mode: 0,
+      date: '2019-11-15',
+      deleted: false
+    },
+    {
+      user: 2,
+      song: 1,
+      score: 108,
+      mode: 0,
+      date: '2019-11-14',
+      deleted: false
+    }
+  ];
