3a60fb58ccdaa375a9565b505be76fc18fd6eb18
soph-iest <sophie.a.debs@gmail.com>
2020-02-21 04:18:07 -0500

    reset DB after each test suite, IDs+dates fix

--- test/features/scores/controller.test.js
+++ test/features/scores/controller.test.js
@@ -1,47 +1,65 @@
+'use strict';
+
 const controller = require('../../../src/features/scores/controller');
+const knex       = require('../../../db/knex');
 const scoreData   = require('../../../db/seeds/data/scores');
 
-test('findScore', async () => {
-    const userId = 1;
-    const songId = 1;
-    expect(await controller.findScore(userId, songId)).toMatchObject(scoreData.find(
-      ({user, song}) => user == userId && song == songId)
-    );
-});
+describe('Scores Controller testing', () => {
 
-test('listByUser', async () => {
-    const userId = 1;
-    expect(await controller.listByUser(userId)).toMatchObject(scoreData.filter(({user}) => user == userId));
-});
+    beforeAll(async (done) => {
+      await knex.migrate.rollback();
+      await knex.migrate.latest();
+      await knex.seed.run();
+      done();
+    });
 
-test('listBySong', async () => {
-    const songId = 1;
-    expect(await controller.listBySong(songId)).toMatchObject(scoreData.filter(({song}) => song == songId));
-});
+    afterAll((done) => {
+        knex.destroy(done);
+    });
 
-let newId;
-const testUser = 2;
-const testSong = 2;
-const newScore = {
-    user: testUser,
-    song: testSong,
-    score: 100,
-    mode: 0
-};
+    test('findScore', async () => {
+        const userId = 1;
+        const songId = 1;
+        expect(await controller.findScore(userId, songId)).toMatchObject(scoreData.find(
+        ({user, song}) => user == userId && song == songId)
+        );
+    });
 
-test('create', async () => {
-    const createdScore = await controller.create(newScore);
-    newId = createdScore.id;
-    expect(createdScore).toMatchObject(newScore);
-});
+    test('listByUser', async () => {
+        const userId = 1;
+        expect(await controller.listByUser(userId)).toMatchObject(scoreData.filter(({user}) => user == userId));
+    });
 
-test('update', async () => {
-    const createdScore = await controller.update(newId, 200);
-    expect(createdScore).toMatchObject({...newScore, score: 200});
-});
+    test('listBySong', async () => {
+        const songId = 1;
+        expect(await controller.listBySong(songId)).toMatchObject(scoreData.filter(({song}) => song == songId));
+    });
 
-test('clear', async () => {
-    expect(await controller.clear(testUser, testSong)).toMatchObject(
-        {id: newId, score: 0}
-    );
+    let newId;
+    const testUser = 2;
+    const testSong = 2;
+    const newScore = {
+        user: testUser,
+        song: testSong,
+        score: 100,
+        mode: 0
+    };
+
+    test('create', async () => {
+        const createdScore = await controller.create(newScore);
+        newId = createdScore.id;
+        expect(createdScore).toMatchObject(newScore);
+    });
+
+    test('update', async () => {
+        const createdScore = await controller.update(newId, 200);
+        expect(createdScore).toMatchObject({...newScore, score: 200});
+    });
+
+    test('clear', async () => {
+        expect(await controller.clear(testUser, testSong)).toMatchObject(
+            {id: newId, score: 0}
+        );
+    });
+
 });
