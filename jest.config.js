da47c76ecb3685b0e2fa4ba65a6b05289c30b2bb
soph-iest <sophie.a.debs@gmail.com>
2020-02-25 18:02:55 -0500

    features specifically

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    "./src/**"
+    "./src/features/**"
   ],
 
   // The directory where Jest should output its coverage files
