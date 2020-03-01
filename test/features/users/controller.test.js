32793205b4d18a6684b18bc8fffc991536df640a
soph-iest <sophie.a.debs@gmail.com>
2020-03-01 09:15:24 -0500

    users fail testing

--- test/features/users/controller.test.js
+++ test/features/users/controller.test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Boom = require('@hapi/boom');
+
 const controller = require('../../../src/features/users/controller');
 const knex       = require('../../../db/knex');
 const userData   = require('../../../db/seeds/data/users');
@@ -18,45 +20,89 @@ describe('Users Controller testing', () => {
     knex.destroy(done);
   });
 
-  test('findById', async () => {
-    expect(await controller.findById(1)).toMatchObject(userData[0]);
+
+  describe('findById', () => {
+    test('returns a user by ID', async () => {
+      expect(await controller.findById(1)).toMatchObject(userData[0]);
+    });
+
+    test('fails safely when searching for a nonexistent user', async () => {
+      const userId = 100;
+      expect(await controller.findById(userId)).toMatchObject(Boom.notFound());
+    });
+
+    test('handles other errors', async () => {
+        const userId = {abcd: 'haha'};
+        const {output} = await controller.findById(userId);
+        expect(output.statusCode).toEqual(400);
+    });
   });
 
-  test('list', async () => {
-      expect(await controller.list()).toMatchObject(userData);
+  describe('list', () => {
+    test('lists all users', async () => {
+        expect(await controller.list()).toMatchObject(userData);
+    });
   });
 
   let newId;
   let newUsername;
 
-  test('create', async () => {
-    const date = new Date();
-    date.setHours(0, 0, 0, 0); // avoid millisecond-errors
-      newUsername = "newuser" + Math.random();
-      const newUser = {
-        email: "newuseremail@google.com" + Math.random(),
-        username: newUsername,
-        password: "password",
-        name: "New User",
-        level: 1,
-        join_date: date
-      };
-      const createdUser = await controller.create(newUser);
-      newId = createdUser.id;
-      expect(createdUser).toMatchObject(newUser);
+  describe('create', () => {
+    test('creates a new user', async () => {
+      const date = new Date();
+      date.setHours(0, 0, 0, 0); // avoid millisecond-errors
+        newUsername = "newuser" + Math.random();
+        const newUser = {
+          email: newUsername,
+          password: "password",
+          name: "New User",
+          join_date: date
+        };
+        const createdUser = await controller.create(newUser);
+        newId = createdUser.id;
+        expect(createdUser).toMatchObject({...newUser, username: newUsername, level: 1});
+    });
+
+    test('handles errors', async() => {
+      const {output} = await controller.create({user: {abcd: 'haha'}});
+      expect(output.statusCode).toEqual(400);
+    });
   });
 
-  test('deleteById', async () => {
-      expect(await controller.deleteById(newId)).toMatchObject(
-          {id: newId, username: newUsername, deleted: true}
+  describe('deleteById', () => {
+    test('soft-deletes a user', async () => {
+        expect(await controller.deleteById(newId)).toMatchObject(
+            {id: newId, username: newUsername, deleted: true}
+        );
+    });
+
+    test('fails safely when attempting to delete a nonexistent user', async () => {
+      const userId = 100;
+      expect(await controller.deleteById(userId)).toMatchObject(Boom.notFound());
+    });
+
+    test('handles other errors', async() => {
+      const {output} = await controller.deleteById({user: {abcd: 'haha'}});
+      expect(output.statusCode).toEqual(400);
+    });
+  });
+
+
+  describe('login', () => {
+    test('checks whether a given username and password match an existing user', async () => {
+      expect(await controller.login(userData[0].username, userData[0].password)).toMatchObject(
+          userData[0]
       );
-  });
+    });
 
+    test('fails safely when attempting to login to a nonexistent user', async () => {
+      expect(await controller.login(800, 800)).toMatchObject(Boom.notFound());
+    });
 
-  test('login', async () => {
-    expect(await controller.login(userData[0].username, userData[0].password)).toMatchObject(
-        userData[0]
-    );
+    test('handles other errors', async() => {
+      const {output} = await controller.login({user: {abcd: 'haha'}});
+      expect(output.statusCode).toEqual(400);
+    });
   });
 
 });
